c1b927f590e460c9eef1a2ac9a0428735e15f3be
stags <stags@xps15>
2025-12-09 12:16:31 -0800

    Enable caffeine extension at startup

--- home/stags/default.nix
+++ home/stags/default.nix
@@ -71,7 +71,10 @@
       toggle-state = true;
     };
     "org/gnome/shell" = {
-      enabled-extensions = [ "appindicatorsupport@rgcjonas.gmail.com" ];
+      enabled-extensions = [
+        "appindicatorsupport@rgcjonas.gmail.com"
+        "caffeine@patapon.info"
+      ];
     };
     "org/gnome/desktop/wm/preferences" = {
       button-layout = "appmenu:minimize,maximize,close";
