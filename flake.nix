4d644aa927e17ca5cba6af9496aa63e23962a6a1
stags <stags@xps15>
2025-12-10 14:49:41 -0800

    Reintroduce host-agnostic dinOS configs

--- flake.nix
+++ flake.nix
@@ -16,6 +16,66 @@
       nixosModules.dinOS = ./modules/dinOS;
       nixosModules.users-stags = ./modules/users/stags.nix;
 
+      nixosConfigurations.dinOS = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [
+          self.nixosModules.dinOS
+          impermanence.nixosModules.impermanence
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
+          {
+            boot.loader.systemd-boot.enable = true;
+            boot.loader.efi.canTouchEfiVariables = nixpkgs.lib.mkDefault false;
+            boot.loader.grub.enable = nixpkgs.lib.mkDefault false;
+            fileSystems."/" = {
+              device = "tmpfs";
+              fsType = "tmpfs";
+              options = [ "mode=0755" "size=2G" ];
+            };
+            networking.hostName = "dinOS";
+            time.timeZone = "America/Los_Angeles";
+            hardware.enableRedistributableFirmware = true;
+            hardware.cpu.intel.updateMicrocode = nixpkgs.lib.mkDefault true;
+            system.stateVersion = "25.11";
+          }
+        ];
+      };
+
+      nixosConfigurations.dinOS-stags = nixpkgs.lib.nixosSystem {
+        inherit system;
+        specialArgs = { inherit inputs; };
+        modules = [
+          self.nixosModules.dinOS
+          self.nixosModules.users-stags
+          impermanence.nixosModules.impermanence
+          agenix.nixosModules.default
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+          }
+          {
+            boot.loader.systemd-boot.enable = true;
+            boot.loader.efi.canTouchEfiVariables = nixpkgs.lib.mkDefault false;
+            boot.loader.grub.enable = nixpkgs.lib.mkDefault false;
+            fileSystems."/" = {
+              device = "tmpfs";
+              fsType = "tmpfs";
+              options = [ "mode=0755" "size=2G" ];
+            };
+            networking.hostName = "dinOS-stags";
+            time.timeZone = "America/Los_Angeles";
+            hardware.enableRedistributableFirmware = true;
+            hardware.cpu.intel.updateMicrocode = nixpkgs.lib.mkDefault true;
+            system.stateVersion = "25.11";
+          }
+        ];
+      };
+
       nixosConfigurations.xps15 = nixpkgs.lib.nixosSystem {
         inherit system;
         specialArgs = { inherit inputs; };
