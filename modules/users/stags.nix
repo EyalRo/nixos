b8eb2e1d97222cac891dd8c8add124d53a8bf6bd
Stags <stags@isdino.com>
2026-02-07 18:29:33 -0800

    users/stags: drop NAS CIFS mount

--- modules/users/stags.nix
+++ modules/users/stags.nix
@@ -1,22 +1,26 @@
-{ config, pkgs, lib, ... }:
+{ config, lib, ... }:
 
 {
   users.users.stags = {
     isNormalUser = true;
     description = "stags";
+    uid = 1026;
+    group = "stags";
     extraGroups = [ "docker" "networkmanager" "wheel" ];
   };
 
-  time.timeZone = lib.mkDefault "America/Los_Angeles";
+  users.groups.stags = {
+    gid = 1026;
+  };
 
-  boot.supportedFilesystems = [ "cifs" ];
+  time.timeZone = lib.mkDefault "America/Los_Angeles";
 
   services.tailscale = {
     enable = true;
     useRoutingFeatures = "client";
   };
 
-  # Persist tailscale state and NAS credentials.
+  # Persist tailscale state.
   environment.persistence."/persist" = {
     directories = [
       "/var/lib/tailscale"
@@ -27,35 +31,6 @@
   };
 
   age.identityPaths = [ "/home/stags/.config/age/keys.txt" ];
-  age.secrets."smb-credentials-nas" = {
-    file = ../../secrets/smb-credentials-nas.age;
-    path = "/persist/etc/smb-credentials-nas";
-    mode = "0600";
-    owner = "root";
-    group = "root";
-  };
-
-  fileSystems."/home/stags/NAS" = {
-    device = "//nas.isdino.com/home";
-    fsType = "cifs";
-    options = let
-      stagsUid = toString config.users.users.stags.uid;
-      stagsGid = toString config.users.groups.${config.users.users.stags.group}.gid;
-    in [
-      "credentials=/persist/etc/smb-credentials-nas"
-      "uid=${stagsUid}"
-      "gid=${stagsGid}"
-      "file_mode=0640"
-      "dir_mode=0750"
-      "vers=3.0"
-      "iocharset=utf8"
-      "x-systemd.automount"
-      "noauto"
-      "x-systemd.idle-timeout=600"
-      "x-systemd.device-timeout=10s"
-      "nofail"
-    ];
-  };
 
   home-manager.users.stags = import ../../home/stags;
 }
